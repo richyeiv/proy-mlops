223176c9dc8727d62486fda75740c32d730f5eea
ric-crc <riccrc@outlook.com>
2023-07-10 00:27:31 -0400

    Fix: Change model and improve prediction

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import os
 import cv2
 import numpy as np
+import keras
 
 app = Flask(__name__)
 
@@ -17,14 +18,18 @@ def predict():
     target_path = os.path.join("upload",new_file.filename)
     new_file.save(target_path)
 
-    image = cv2.imread(target_path, 0)
+    image = cv2.imread(target_path, 1)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = data_validation(image)
 
-    with open("ml/MLP.pkl", 'rb') as file:
-            classifier = pickle.load(file)
-    prediction = classifier.predict(image)
+    classifier = keras.models.load_model("ml/modelovgg_grupo3.h5",  compile=False)
+    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    clase_predicha = round(prediction.tolist()[0])
+    dummy = np.array([[1,1,1]])
+    
+    prediction = classifier.predict([dummy, image])
+    print(prediction)
+    clase_predicha = round(prediction[0][0])
 
     # Imprimir el resultado de la predicción
     if clase_predicha == 0:
@@ -36,7 +41,6 @@ def predict():
 def data_validation(image):
     IMAGE_SIZE = (224,224)
     image = cv2.resize(image, IMAGE_SIZE)
-    image = image.flatten()
     image = np.expand_dims(image, axis=0)
     return image
 
